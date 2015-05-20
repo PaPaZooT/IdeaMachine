c997093041bb996792f4320797345f7096b01e10
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2015-05-20 18:06:03 +0200

    tags now sorted by date created

--- app/src/main/java/hr/matvidako/ideamachine/tag/storage/DatabaseTagStorage.java
+++ app/src/main/java/hr/matvidako/ideamachine/tag/storage/DatabaseTagStorage.java
@@ -2,8 +2,11 @@ package hr.matvidako.ideamachine.tag.storage;
 
 import android.content.Context;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import hr.matvidako.ideamachine.db.Data;
 import hr.matvidako.ideamachine.db.Repository;
 import hr.matvidako.ideamachine.tag.Tag;
 
@@ -11,6 +14,20 @@ public class DatabaseTagStorage extends Repository<Tag> implements TagStorage {
 
     public DatabaseTagStorage(Context ctx) {
         super(ctx, Tag.class);
+    }
+
+    @Override
+    public List<Tag> getAll() {
+        return getAllSortedByDateCreatedDesc();
+    }
+
+    private List<Tag> getAllSortedByDateCreatedDesc() {
+        try {
+            return dao.queryBuilder().orderBy(Data.Columns.dateCreated, false).query();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 
 }
